e95169a841c93d539337025465034c88e3797bd3
`Thomas <`pontom@gmx.at>
2021-10-08 19:53:27 +0200

    variablen festgelegt

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,18 @@
 public class Calculator {
 
     public static void main(String[] args) {
+       int thomas = 1;
+       boolean karin = false;
+       double a = 3;
+       char x = 'f';
+       String Gedicht = "versuch" ;
+
+        System.out.println( thomas );
+        System.out.println( karin );
+        System.out.println( a );
+        System.out.println( x );
+        System.out.println(Gedicht);
+
 
     }
 }
